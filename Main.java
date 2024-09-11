c23e1fb5c058af847648c1fb9431c5b29bf2b1df
John Mark Sia <jonhmarksia@gmail.com>
2024-09-11 20:06:39 +0800

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BankSystem bankSystem = new BankSystem();
+        Scanner scanner = new Scanner(System.in);
+        User loggedInUser = null;
+        Account userAccount = null;
+
+        while (true) {
+            System.out.println("\nWelcome to You Connect!");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            System.out.println("Choose an option: ");
+        }
+    }    
+}
